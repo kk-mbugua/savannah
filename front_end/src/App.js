b3989a22466fed01edcf9bc6785c74659fbc66f1
Kamau Mbugua <kamau.dev@gmail.com>
2024-04-25 07:44:12 +0300

    adapted to general layout structure

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,15 +1,14 @@
 import "./assets/styles/App.css"
 import "./assets/styles/global.css"
 import Home from './pages/Home';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+      <Layout>
+        <Home />
+      </Layout>
     </div>
   );
 }
